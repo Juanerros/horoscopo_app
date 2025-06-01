d6495a02e83afce6e2179dc42813887f06110af7
Juanerros <138302286+Juanerros@users.noreply.github.com>
2025-06-01 20:25:06 -0300

    Primer commit

---
+++ app/src/main/java/com/example/horoscopoxml/activitys/lookHoroscopo.kt
@@ -0,0 +1,21 @@
+package com.example.horoscopoxml.activitys
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.horoscopoxml.R
+
+class lookHoroscopo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_look_horoscopo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
